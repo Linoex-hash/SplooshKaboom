15e312264ade308af9cbfd032ab2cb96bf38f700
cpneppal <cpneppal@buffalo.edu>
2020-10-24 18:38:20 -0400

    final commit 2

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ void resetCharacterArray(char a[], int length){
 }
 int main(int argc, char const *argv[]) {
 	srand(time(0));
-	printf("Welcome to sploosh kaboom.\nThis game is based off of Legend of Zelda: The Wind Waker.\nThe rules are to shoot at the grid until all the squids are found. Enjoy!\n");
+	printf("  Welcome to sploosh kaboom.\n  This game is based off of Legend of Zelda: The Wind Waker.\n  The rules are to shoot at the grid until all the squids are found. Enjoy!\n");
 
 	char **grid = gengrid();
 	Point **fourSquid = generate4squid();
@@ -40,115 +40,115 @@ int main(int argc, char const *argv[]) {
 	for(; i < ATTEMPTS && hits < SQUIDFOUR + SQUIDTHREE + SQUIDTWO; ++i){
 		printOutputOfGame(grid, ATTEMPTS - i);
 		if(i <= 0){
-			printf("chosen x: not chosen yet\n");
-			printf("chosen y: not chosen yet\n");
+			printf("  chosen x: not chosen yet\n");
+			printf("  chosen y: not chosen yet\n");
 		}
 		else{
-			printf("chosen x: %d\n", currentX);
-			printf("chosen y: %d\n", currentY);
+			printf("  chosen x: %d\n", currentX);
+			printf("  chosen y: %d\n", currentY);
 		}
 		switch (instanceOutput){
 			case KABOOM: 
-				printf("KABOOM!\n");
+				printf("  KABOOM!\n");
 				break;
 			case SPLOOSH:
-				printf("SPLOOSH\n");
+				printf("  SPLOOSH\n");
 				break;
 			default: 
 				//don't do anything
 				break;
 		}
-		printf("Please enter x coordinate or press q to quit: ");
+		printf("  Please enter x coordinate or press q to quit: ");
 		//reset the character array
 		resetCharacterArray(inputX, 20);
 		//invalid inputs aren't numbers, are less than or equal to 0 (1 - GRIDSIZE), or greater than GRIDSIZE
 		scanf("%s", inputX);
 		if(strlen(inputX) == 1 && inputX[0] == 'q'){
 			//if input is q, then quit
-			puts("quitting...");
+			puts("  quitting...");
 			return 0;
 		}
 		while((currentX = atoi(inputX)) < 1 || currentX > GRIDSIZE){
 			//invalid input
-			puts("coordinate x not valid. Please try again");
+			puts("  coordinate x not valid. Please try again");
 			//reset the character array
 			resetCharacterArray(inputX, 20);
-			printf("Please enter x coordinate or press q to quit: ");
+			printf("  Please enter x coordinate or press q to quit: ");
 			scanf("%s", inputX);
 			if(strlen(inputX) == 1 && inputX[0] == 'q'){
 				//if input is q, then quit
-				puts("quitting...");
+				puts("  quitting...");
 				return 0;
 			}
 		}
-		printf("Please enter y coordinate or press q to quit: ");
+		printf("  Please enter y coordinate or press q to quit: ");
 		//while this input isn't a valid number. If q is entered, return 0 and stop the game
 		resetCharacterArray(inputY, 20);
 		//invalid inputs aren't numbers, are less than or equal to 0 (1 - GRIDSIZE), or greater than GRIDSIZE
 		scanf("%s", inputY);
 		if(strlen(inputY) == 1 && inputY[0] == 'q'){
 			//if input is q, then quit
-			puts("quitting...");
+			puts("  quitting...");
 			return 0;
 		}
 		while((currentY = atoi(inputY)) < 1 || currentY > GRIDSIZE){
 			//invalid input
-			puts("coordinate y not valid. Please try again");
+			puts("  coordinate y not valid. Please try again");
 			//reset the character array
-			printf("Please enter y coordinate or press q to quit: ");
+			printf("  Please enter y coordinate or press q to quit: ");
 			resetCharacterArray(inputY, 20);
 			scanf("%s", inputY);
 			if(strlen(inputY) == 1 && inputY[0] == 'q'){
 				//if input is q, then quit
-				puts("quitting...");
+				puts("  quitting...");
 				return 0;
 			}
 		}
 		while((instanceOutput = splooshKaboom(grid, fourSquid, threeSquid, twoSquid, currentX - 1, currentY - 1)) == 0){
-			printf("Please enter x coordinate or press q to quit: ");
+			printf("  Please enter x coordinate or press q to quit: ");
 			//reset the character array
 			resetCharacterArray(inputX, 20);
 			//scan the input
 			scanf("%s", inputX);			
 			if(strlen(inputX) == 1 && inputX[0] == 'q'){
 				//if input is q, then quit
-				puts("quitting...");
+				puts("  quitting...");
 				return 0;
 			}
 			while((currentX = atoi(inputX)) < 1 || currentX > GRIDSIZE){
 				//invalid input
-				puts("coordinate x not valid. Please try again");
+				puts("  coordinate x not valid. Please try again");
 				//reset the character array
 				resetCharacterArray(inputX, 20);
-				printf("Please enter x coordinate or press q to quit: ");
+				printf("  Please enter x coordinate or press q to quit: ");
 				scanf("%s", inputX);
 				if(strlen(inputX) == 1 && inputX[0] == 'q'){
 					//if input is q, then quit
-					puts("quitting...");
+					puts("  quitting...");
 					return 0;
 				}
 			}
 
-			printf("Please enter y coordinate or press q to quit: ");
+			printf("  Please enter y coordinate or press q to quit: ");
 			//while this input isn't a valid number. If q is entered, return 0 and stop the game
 			resetCharacterArray(inputY, 20);
 			//invalid inputs aren't numbers, are less than or equal to 0 (1 - GRIDSIZE), or greater than GRIDSIZE
 			scanf("%s", inputY);
 			if(strlen(inputY) == 1 && inputY[0] == 'q'){
 				//if input is q, then quit
-				puts("quitting...");
+				puts("  quitting...");
 				return 0;
 			}
 			while((currentY = atoi(inputY)) < 1 || currentY > GRIDSIZE){
 				//invalid input
-				puts("coordinate y not valid. Please try again");
+				puts("  coordinate y not valid. Please try again");
 				//reset the character array
-				printf("Please enter y coordinate or press q to quit: ");
+				printf("  Please enter y coordinate or press q to quit: ");
 				resetCharacterArray(inputY, 20);
 				scanf("%s", inputY);
 				if(strlen(inputY) == 1 && inputY[0] == 'q'){
 					//if input is q, then quit
-					puts("quitting...");
+					puts("  quitting...");
 					return 0;
 				}
 			}
@@ -161,12 +161,12 @@ int main(int argc, char const *argv[]) {
 	//LOST
 	if(hits < SQUIDFOUR + SQUIDTHREE + SQUIDTWO){
 		printAnswerOfGame(grid,fourSquid,threeSquid,twoSquid);
-		printf("You lost the game. Please restart application to try again.\n");
+		printf("  You lost the game. Please restart application to try again.\n");
 	}
 	//WON
 	else{
 		printOutputOfGame(grid, ATTEMPTS - i);
-		printf("You won the game! congradulations!\n");
+		printf("  You won the game! congradulations!\n");
 	}
 
 
